d0ab9dc9ce40d972ece570512b17270f2fd60278
ricardodelimaaa <142522306+ricardodelimaaa@users.noreply.github.com>
2024-01-03 19:55:57 +0000

    personajesyplanetas

---
+++ src/js/component/personajes.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.js"
+
+export const Personajes = () => {
+    const { store, actions } = useContext(Context)
+
+    useEffect(() => {
+        actions.obtenerPersonajes()
+    }, [])
+    return (
+        <div>
+            <h1> Personajes  </h1>
+            <div className="card-group">
+                <div className="d-flex flex-row overflow-scroll">
+                    {store.personajes.map((items,id)=> (
+
+                   
+                    <div key={id} className="card" style={{ width: "18rem", flex: "none", margin: "10px" }}>
+                        <img src={"https://starwars-visualguide.com/assets/img/characters/"+(id+1)+".jpg"} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{items.name}</h5>
+                            <p className="card-text">  gender: {items.gender} </p>
+                            <p className="card-text">  mass: {items.mass} </p>
+                            <a href="#" className="btn btn-primary"> more info </a>
+                        </div>
+                    </div>
+                     ))}
+                </div>
+            </div>
+        </div>
+
+    );
+};
---
+++ src/js/component/planetas.js
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.js"
+
+export const Planetas = () => {
+    const { store, actions } = useContext(Context)
+
+    useEffect(() => {
+        actions.obtenerPlanetas()
+    }, [])
+    return (
+        <div>
+            <h1> Planetas  </h1>
+            <div className="card-group">
+                <div className="d-flex flex-row overflow-scroll">
+                    {store.planetas.map((items,id)=> (
+
+                   
+                    <div key={id} className="card" style={{ width: "18rem", flex: "none", margin: "10px" }}>
+                        {id==0?
+                        <img src={"https://static.wikia.nocookie.net/esstarwars/images/b/b0/Tatooine_TPM.png"} className="card-img-top" alt="..." />:
+
+                        <img src={"https://starwars-visualguide.com/assets/img/planets/"+(id+1)+".jpg"} className="card-img-top" alt="..." />
+                    }
+                        <div className="card-body">
+                            <h5 className="card-title">{items.name}</h5>
+                            <p className="card-text">  climate: {items.climate} </p>
+                            <p className="card-text">  gravity: {items.gravity} </p>
+                            <a href="#" className="btn btn-primary"> more info </a>
+                        </div>
+                    </div>
+                     ))}
+                </div>
+            </div>
+        </div>
+
+    );
+};
